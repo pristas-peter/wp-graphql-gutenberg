9e1efd393bc3cfdbabefbb87386b411cd53f6a8a
Peter Pristas <pristas.peter@gmail.com>
2020-04-27 12:56:38 +0200

    Add initial docs version for the upcoming release

---
+++ docs/src/gatsby-theme-apollo-core/components/logo.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Logo() {
+	return (
+		<p
+			style={{
+				display: "block",
+				height: "auto",
+			}}
+		>
+			🚀 wp-graphql-gutenberg
+		</p>
+	)
+}
