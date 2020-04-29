26b1a888ebf87862a0a99455337151f532b86450
Peter Pristas <pristas.peter@gmail.com>
2020-04-29 19:16:55 +0200

    Fix connection resolver

--- src/Schema/Types/Connection/BlockEditorContentNodeConnection.php
+++ src/Schema/Types/Connection/BlockEditorContentNodeConnection.php
@@ -5,46 +5,34 @@ namespace WPGraphQLGutenberg\Schema\Types\Connection;
 use WPGraphQL\Connection\PostObjects;
 use WPGraphQLGutenberg\Schema\Utils;
 
-class BlockEditorContentNodeConnection
-{
-    function __construct()
-    {
-        add_action(
-            'graphql_register_types',
-            function ($type_registry) {
+class BlockEditorContentNodeConnection {
+	function __construct() {
+		add_action('graphql_register_types', function ( $type_registry ) {
+			register_graphql_connection([
+				'fromType'           => 'RootQuery',
+				'toType'             => 'BlockEditorContentNode',
+				'fromFieldName'      => 'blockEditorContentNodes',
+				'connectionArgs'     => PostObjects::get_connection_args(),
+				'connectionTypeName' => 'BlockEditorContentNodeConnection',
+				'resolve'            => function ( $id, $args, $context, $info ) {
+					$resolver = new \WPGraphQL\Data\Connection\PostObjectConnectionResolver(
+						$id,
+						$args,
+						$context,
+						$info,
+						'post'
+					);
+					$resolver->setQueryArg( 'post_type', Utils::get_graphql_allowed_editor_post_types() );
+					$connection = $resolver->get_connection();
+					return $connection;
+				},
+				'resolveNode'        => function ( $model, $args, $context, $info ) {
+					$id = $model->ID ?? $model;
 
-                register_graphql_connection([
-                    'fromType' => 'RootQuery',
-                    'toType' => 'BlockEditorContentNode',
-                    'fromFieldName' => 'blockEditorContentNodes',
-                    'connectionArgs' => PostObjects::get_connection_args(),
-                    'connectionTypeName' => 'BlockEditorContentNodeConnection',
-                    'resolve' => function ($id, $args, $context, $info) {
-                        $resolver = new \WPGraphQL\Data\Connection\PostObjectConnectionResolver(
-                            $id,
-                            $args,
-                            $context,
-                            $info,
-                            'post'
-                        );
-                        $resolver->setQueryArg(
-                            'post_type',
-                            Utils::get_graphql_allowed_editor_post_types()
-                        );
-                        $connection = $resolver->get_connection();
-                        return $connection;
-                    },
-                    'resolveNode' => function (
-                        $model,
-                        $args,
-                        $context,
-                        $info
-                    ) {
-                        $resolver = Utils::get_post_resolver($model->ID);
-                        return $resolver($model->ID, $context);
-                    }
-                ]);
-            }
-        );
-    }
+					$resolver = Utils::get_post_resolver( $id );
+					return $resolver( $id, $context );
+				},
+			]);
+		});
+	}
 }
