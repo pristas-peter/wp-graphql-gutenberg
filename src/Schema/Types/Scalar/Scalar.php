370d5386bd1c19d0304dcedf3f6823ce08a93b25
Peter Pristas <pristas.peter@gmail.com>
2020-05-22 00:18:30 +0200

    Update scalar registration method with upstream changes

--- src/Schema/Types/Scalar/Scalar.php
+++ src/Schema/Types/Scalar/Scalar.php
@@ -2,34 +2,28 @@
 
 namespace WPGraphQLGutenberg\Schema\Types\Scalar;
 
-class Scalar
-{
-	public function __construct()
-	{
+class Scalar {
+	public function __construct() {
 		add_action('graphql_register_types', function ($type_registry) {
-			register_graphql_scalar([
-				'name'      => 'BlockAttributesObject',
+			register_graphql_scalar('BlockAttributesObject', [
 				'serialize' => function ($value) {
 					return json_encode($value);
-				},
+				}
 			]);
 
-			register_graphql_scalar([
-				'name'      => 'BlockAttributesArray',
+			register_graphql_scalar('BlockAttributesArray', [
 				'serialize' => function ($value) {
 					return json_encode($value);
-				},
+				}
 			]);
 		});
 	}
 
-	public static function BlockAttributesObject()
-	{
+	public static function BlockAttributesObject() {
 		return 'BlockAttributesObject';
 	}
 
-	public static function BlockAttributesArray()
-	{
+	public static function BlockAttributesArray() {
 		return 'BlockAttributesArray';
 	}
 }
