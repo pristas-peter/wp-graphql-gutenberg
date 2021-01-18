63f99194c25afdd8d31484f0bf159d7f4168b9b4
pristas-peter <pristas.peter@gmail.com>
2021-01-18 06:45:43 +0100

    Bump version to 0.3.7

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@
  * Description: Enable blocks in WP GraphQL.
  * Author: pristas-peter
  * Author URI:
- * Version: 0.3.5
+ * Version: 0.3.7
  * Requires at least: 5.4
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
